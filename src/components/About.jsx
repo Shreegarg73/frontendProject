32282388da7a9450ceb840d39bb812a6a338c642
Shreya <117116843+Shreegarg73@users.noreply.github.com>
2024-04-25 09:37:42 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,17 +6,7 @@ const About=()=>{
           <h4>Events</h4>
           <h2>ABOUT</h2>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet facilis
-            voluptas provident animi corrupti dolor, ipsum deleniti perferendis
-            veniam neque labore enim odio ratione. Ad commodi rem corporis soluta
-            sed reprehenderit a quasi nihil, odit aperiam exercitationem? Suscipit
-            rerum quibusdam inventore atque veritatis accusantium excepturi,
-            molestias optio perferendis aliquam harum, amet dolore! Animi
-            consequuntur officiis modi eius cumque reiciendis harum ipsum
-            praesentium numquam fugit? Consequatur eveniet recusandae harum hic. Eum
-            illo praesentium optio accusantium, hic quibusdam delectus voluptatem
-            qui a officiis dolorum minima cum illum culpa doloremque quia quidem aut
-            sed laudantium nam repudiandae sequi similique! Esse natus at quas?
+            An event management company specializes in planning, organizing, and executing various events, from corporate conferences to weddings. They offer services such as venue selection, budget management, catering, entertainment booking, and logistics coordination. Their goal is to ensure events run smoothly by creating detailed plans, managing vendors, and handling any issues that arise.
           </p>
         </section>
       );
